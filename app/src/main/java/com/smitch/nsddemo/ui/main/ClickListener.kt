329381758ff70bfebd91af0396d9ccafaaea4551
Elangovan <elangovank2602@gmail.com>
2021-11-28 10:12:15 +0530

    MVVM and Data basic binding setup _1

---
+++ app/src/main/java/com/smitch/nsddemo/ui/main/ClickListener.kt
@@ -0,0 +1,8 @@
+package com.smitch.nsddemo.ui.main
+
+import android.view.View
+
+interface ClickListener {
+
+    fun onClicked(view : View)
+}
--- app/src/main/java/com/smitch/nsddemo/ui/main/MainActivity.kt
+++ app/src/main/java/com/smitch/nsddemo/ui/main/MainActivity.kt
@@ -2,26 +2,41 @@ package com.smitch.nsddemo.ui.main
 
 import android.content.Context
 import android.os.Bundle
+import android.view.View
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import androidx.databinding.DataBindingUtil
 import androidx.lifecycle.ViewModelProvider
 import com.smitch.nsddemo.R
+import com.smitch.nsddemo.databinding.ActivityMainBinding
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), ClickListener {
     lateinit var mainViewModel: MainViewModel
     private lateinit var viewModelFactory: MainViewModel.Factory
-    private val context: Context by lazy { this }
+    private val context: MainActivity by lazy { this }
+
+    private lateinit var mainActivityBinding: ActivityMainBinding
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        mainActivityBinding = DataBindingUtil. setContentView(context, R.layout.activity_main)
+       mainActivityBinding.scan = context
         setupViewModel()
         mainViewModel.print()
     }
 
     private fun setupViewModel() {
         viewModelFactory = MainViewModel.Factory(context)
-        mainViewModel = ViewModelProvider(this, viewModelFactory)
+        mainViewModel = ViewModelProvider(context, viewModelFactory)
             .get(MainViewModel::class.java)
 
+    }
+    override fun onClicked(view : View){
+         printToast("Scan clicked")
+    }
+
+    private fun printToast(msg: String){
+        Toast.makeText(context,msg, Toast.LENGTH_SHORT).show()
     }
 }
